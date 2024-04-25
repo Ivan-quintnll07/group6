0b5418ef44f651a78298d0733fc32ec67a22d5c7
Sofia Landaverde <153237783+S0FI-LANDAVERDE@users.noreply.github.com>
2024-04-25 15:54:51 -0600

    Create migrations, seeders, factories and models

--- database/migrations/2024_04_25_213527_create_cards_table.php
+++ database/migrations/2024_04_25_213527_create_cards_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('_user', function (Blueprint $table) {
+        Schema::create('cards', function (Blueprint $table) {
             $table->id();
-            $table->string('User');
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('_user');
+        Schema::dropIfExists('cards');
     }
 };
