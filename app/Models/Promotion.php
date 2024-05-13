57c6abd1c78b845434c9595109caa0803c43b107
Elías Flores <elias.fflores2008@gmail.com>
2024-05-13 16:25:54 -0600

    Models updated

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -8,7 +8,7 @@ use App\Models\CardType;
 
 
 
-class CardType extends Model
+class Promotion extends Model
 {
     use HasFactory;
     public function CardType() {
--- app/Models/calendar.php
+++ app/Models/calendar.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Card;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Calendar extends Model
 {
     use HasFactory;
+    public function Card(){
+        return $this->belongsTo(Card::class);
+
+    }
 }
+
+
+{
+    public function Card(){
+
+        return $this->belongsTo(User::class);
+    }
+}
+
