3f92a3bb1c4cb8e292d02f5332fc3e0ea3854370
Elías Flores <elias.fflores2008@gmail.com>
2024-06-04 15:27:31 -0600

    homepage blade

--- resources/views/users/homepage.blade.php
+++ resources/views/users/homepage.blade.php
@@ -1,6 +1,13 @@
 <x-layout>
     <div>
         <p>Fecha de expiracion</p>
-        <span>25/6/9</span>
+        <span>25/6/2029</span>
+        <p> Fecha de corte </p>
+        <span> 26/6/2024</span>
+        <p> Tipo de tarjeta </p>
+        <p> Nombre del banco </p>
+        <p> Nombre de usuario </p>
+
+
     </div>
 </x-layout>
