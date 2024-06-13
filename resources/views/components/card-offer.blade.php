7d0158ce48b3d09e9fd938c5e8359c61c57fa6a3
Elías Flores <elias.fflores2008@gmail.com>
2024-06-13 15:56:55 -0600

    Offer

---
+++ resources/views/components/card-offer.blade.php
@@ -0,0 +1,20 @@
+<div class="grid grid-cols-2 space-x-5 bg-slate-200 shadow-md ;">
+
+    <div class="flex flex-col items-center justify-center p-2">
+        <h1> Por una quesadilla y un café de 9oz por  </h1>
+        <p> $2.99 </p>
+        <img src="{{ asset('img/Juan Valdez Café.png')}}">
+
+    </div>
+
+    <div class="bg-cover bg-center bg-no-repeat" style="background-image: url({{ asset('img/cafe.jpg') }})">
+
+
+    </div>
+
+    <div class="col-span-2 p-1">
+        <p>Comparte con tus amigos una tarde de café 
+            Acompaña una deliciosa taza de café  con una quesadilla a un ¡súper precio! al pagar con tu Tarjeta Crédito o Débito. Válido desde el 1 al 31 de mayo.
+        </p>
+    </div>
+</div>
--- resources/views/offers/offer.blade.php
+++ resources/views/offers/offer.blade.php
@@ -1,1 +1,18 @@
+<x-layout>
+    <div class="flex flex-col bg-cover bg-center">
 
+        <header class="bg-gray-200 p-4 flex justify-between items-center shadow-md">
+            <span class="material-icons-outlined cursor-pointer" id="menuButton"> menu </span>
+            <span class="flex justify-items-center items-center "> Ofertas </span>
+            <span class="material-icons-outlined"> account_circle </span>
+
+        </header>
+
+    </div>
+
+    <div class="grid grid-cols-4 my-7">
+        <h1 class="col-span-1">Restaurantes</h1>
+        <x-card-offer></x-card-offer>
+    </div>
+
+</x-layout>
