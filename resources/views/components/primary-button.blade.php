b55c44e83dedcd42561491161a10bcb515da2432
manlikebr1m <145821212+manlikebr1m@users.noreply.github.com>
2024-06-25 13:52:22 -0600

    ahorita

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,7 +1,7 @@
-<div class="px-20 justify-center">
-<button
-    class="bg-blue-500 hover:bg-gray-700 text-gray font-bold rounded-full focus:outline-none focus:shadow-outline"
+<div class="text-center">
+    <button
+    class="bg-blue-500 hover:bg-gray-700 text-gray font-bold rounded-full px-12 py-3 focus:outline-none focus:shadow-outline"
     type="submit">
     ENVIAR
-</button>
+    </button>
 </div>
