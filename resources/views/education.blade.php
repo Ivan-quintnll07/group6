8733ab4e5ed29c924f92863e766af2fd540ee013
Iván Quintanilla <153476509+Ivan-quintnll07@users.noreply.github.com>
2024-07-08 15:49:09 -0600

    Education page

--- resources/views/education.blade.php
+++ resources/views/education.blade.php
@@ -1,3 +1,52 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-</div>
+<x-layout>
+
+    <h1 style="color: white; font-family: 'Kodchasan', sans-serif; text-align: center; background-color: #384E77; padding: 10px; box-shadow: 5px 5px 10px gray;font-size: 35px">Educación Financiera</h1>
+    <div class="bg-[#eee] text-black p-10">
+
+        <div class="flex items-center justify-center min-h-screen container mx-auto">
+
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+
+                <div class="card">
+                    <div>
+                        <div class="p-5 flex flex-col">
+                            <div class="rounded-xl overflow-hidden">
+                                <img src="https://previews.123rf.com/images/artcuboy/artcuboy2306/artcuboy230600677/207424797-ilustraci%C3%B3n-de-arte-digital-del-bosque-encantado.jpg" alt="" />
+                            </div>
+                            <h5 class="text-2xl md:text-3xl font-medium mt-3">Clases de tarjetas</h5>
+                            <p>el siguiente video muestra bla bla bla bla bla</p>
+                            <a href="" class="text-center bg-blue-400 text-blue-700 py-2 rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">video</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <div class="p-5 flex flex-col">
+                            <div class="rounded-xl overflow-hidden">
+                                <img src="https://www.visa.com.sv/dam/VCOM/regional/lac/SPA/Default/Pay%20With%20Visa/Find%20a%20Card/Credit%20cards/Classic/visa_classic_card_400x225.jpg" alt="" />
+                            </div>
+                            <h5 class="text-2xl md:text-3xl font-medium mt-3">Como ahorrar</h5>
+                            <p>el siguiente video muestra bla bla bla bla bla</p>
+                            <a href="" class="text-center bg-blue-400 text-blue-700 py-2 rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">video</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div>
+                        <div class="p-5 flex flex-col">
+                            <div class="rounded-xl overflow-hidden">
+                                <img src="https://cam.mastercard.com/content/dam/public/mastercardcom/lac/mx/home/empresas/empresas-grandes/tarjetas/corporate/tarjeta-corporate-1280x720.jpg" alt="" />
+                            </div>
+                            <h5 class="text-2xl md:text-3xl font-medium mt-3">Uso de tus tarjetas</h5>
+                            <p>el siguiente video muestra bla bla bla bla bla</p>
+                            <a href="" class="text-center bg-blue-400 text-blue-700 py-2 rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">video</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+</x-layout>
