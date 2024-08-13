7b56736de086e09f0a8cef4a450da76c09d1c3b4
Sofia Landaverde <153237783+S0FI-LANDAVERDE@users.noreply.github.com>
2024-08-13 14:56:58 -0600

    COLORS

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center">
     <button
-    class="bg-blue-500 hover:bg-gray-700 text-gray font-bold rounded-full px-12 py-3 focus:outline-none focus:shadow-outline"
+    class="bg-blue-500 hover:bg-gray-700 text-gray font-poppins rounded-full px-12 py-3 focus:outline-none focus:shadow-outline"
     type="submit">
     ENVIAR
     </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,17 @@ export default {
             colors: {
                 click: {
                     'primaryYellow': '#EDDE58',
-                    'blue': '',
+                    'blue1': '#18314F',
+                    'blue2': '#38477',
+                    'blue3': '#0051F5',
+                    'blue4': '#12B6F5',
+                    'greenlime': '#E6F9AF',
+
                 }
             },
             fontFamily: {
                 'poppins': ['Poppins', ...defaultTheme.fontFamily.sans],
-                
+
             }
         },
         plugins: [],
