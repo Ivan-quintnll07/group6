d1747c79dd728e1cd6cc3f3bd925a307c255badf
Elías Flores <elias.fflores2008@gmail.com>
2024-08-13 14:59:21 -0600

    offer 1.8

--- resources/views/components/card-offer.blade.php
+++ resources/views/components/card-offer.blade.php
@@ -1,16 +1,14 @@
 @props(['title', 'price', 'description', 'image', 'logo'])
 
-<div class="grid grid-cols-2 bg-slate-200 shadow-md ">
+<div class="grid grid-cols-2 bg-slate-200 shadow-md mx-2 my-2">
     <div class="flex flex-col items-center justify-center p-1  ">
         <h1> {{ $title }}</h1>
         <p class="text-4xl p-1"> {{ $price }} </p>
         <img src="{{ asset("img/$logo") }}" class="mt-2 h-12">
-
     </div>
 
     <div class="flex flex-col items-center justify-center">
-
-    <img src="{{ asset("img/$image") }}" class="mt-2 h-35">
+        <img src="{{ asset("img/$image") }}" class="mt-2 h-35">
     </div>
 
     <div class="col-span-2 p-4 items-center justify-center">
