b0561883f7d2c3dbab004b07e493ec4436929a45
manlikebr1m <145821212+manlikebr1m@users.noreply.github.com>
2024-09-05 14:19:58 -0600

    imagenes login register

--- resources/views/cards/form.blade.php
+++ resources/views/cards/form.blade.php
@@ -1,6 +1,8 @@
 <x-layout>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <div class="flex flex-col h-screen bg-cover bg-center bg-['{{asset('img/bg/creditCard.jpg')}}']">
+    <div class="flex flex-col h-screen bg-cover bg-center bg-['url({{asset('img/bg/creditCard.jpg')}})']"
+            style="background-image: url('img/bg/creditCard.jpg')"
+        >
         <form action="/cards/create" method="POST">
             @csrf
 
