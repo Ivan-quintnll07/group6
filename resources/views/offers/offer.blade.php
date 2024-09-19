e1c44f70dc3f38fee22fb27f68abef133f9cfab9
Sofia Landaverde <153237783+S0FI-LANDAVERDE@users.noreply.github.com>
2024-09-19 14:02:26 -0600

    Offers

--- resources/views/offers/offer.blade.php
+++ resources/views/offers/offer.blade.php
@@ -1,12 +1,18 @@
 <x-layout>
     <x-bar></x-bar>
 
-    <div class="w-full my-4 ">
-        <h1 class="w-64 flex items-center justify-center h-[calc(100vh-75px)] mx-4 fixed">Promociones</h1>
+    <div class="w-full my-8 ">
+        <h1 class="text-4xl font-semibold text-center mb-10">Promociones</h1>
 
-        <div class="grid grid-cols-3 w-3/4 ms-64">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-8">
             @foreach ($offers as $offer)
-                <x-card-offer title='{{ $offer -> title}}' price='{{$offer -> price}}' image='{{$offer -> image}}' logo='{{$offer -> logo}}' description='{{$offer -> benefit}}'/>
+                <x-card-offer
+                    title='{{ $offer->title }}'
+                    price='{{ $offer->price }}'
+                    image='{{ $offer->image }}'
+                    logo='{{ $offer->logo }}'
+                    description='{{ $offer->benefit }}'
+                    page='{{ asset("/pages/offer-{$offer->id}.html") }}' />
             @endforeach
         </div>
     </div>
