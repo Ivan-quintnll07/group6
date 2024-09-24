bd494db189e2219faae52dfa1b8e53f7ee449726
manlikebr1m <145821212+manlikebr1m@users.noreply.github.com>
2024-09-23 20:01:09 -0600

    iconos en bar

--- resources/views/components/bar.blade.php
+++ resources/views/components/bar.blade.php
@@ -1,5 +1,9 @@
 <body class="bg-gray-100">
     <header class="bg-click-primaryYellow p-4 shadow-md flex justify-between items-center">
+        <head>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+          </head>
+
         <div class="flex items-center">
             <span class="material-symbols-outlined cursor-pointer" id="menuButton">
                 menu
@@ -27,23 +31,45 @@
                 <img src="{{ asset('img/Principal.png') }}" width="140" height="10" aling="center">
 
                 @guest
-                <b><a href="/" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Página Principal</a></b>
-                <b><a href="signup" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Registrarse</a></b>
-                <b><a href="login" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Iniciar Sesión</a></b>
-                <b><a href="calendar" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Calendario</a></b>
-                <b><a href="addCard" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Agregar Tarjeta</a></b>
-                <b><a href="education" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Educacion </a></b>
-                <b><a href="offers" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Ofertas</a></b>
+                <b><a href="/" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-home"></i> Página Principal</a></b>
 
+                <b><a href="signup" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-user-plus"></i> Registrarse</a></b>
+
+                <b><a href="login" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-sign-in-alt"></i> Iniciar Sesión</a></b>
+
+                <b><a href="calendar" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-calendar-alt"></i> Calendario</a></b>
+
+                <b><a href="addCard" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-credit-card"></i> Agregar Tarjeta</a></b>
+
+                <b><a href="education" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-graduation-cap"></i> Educación</a></b>
+
+                <b><a href="offers" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-tags"></i> Ofertas</a></b>
                 @endguest
 
                 @auth
-                <b><a href="/" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Página Principal</a></b>
-                <b><a href="calendar" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Calendario</a></b>
-                <b><a href="education" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Educación </a></b>
-                <b><a href="addCard" class="block text-slate-950 hover:text-gray-100 text-xl h-9">Agregar Tarjeta</a></b>
-                <b><a href="offers" class="text-slate-950 hover:text-gray-100 text-xl h-9">Ofertas</a></b>
+                <b><a href="/" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-home"></i> Página Principal</a></b>
+
+                <b><a href="calendar" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-calendar-alt"></i> Calendario</a></b>
+
+                <b><a href="education" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-graduation-cap"></i> Educación</a></b>
+
+                <b><a href="addCard" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-credit-card"></i> Agregar Tarjeta</a></b>
+
+                <b><a href="offers" class="block text-slate-950 hover:text-gray-100 text-xl h-9">
+                    <i class="fas fa-tags"></i> Ofertas</a></b>
                 @endauth
+
 
 
             </nav>
