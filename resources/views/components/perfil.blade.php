81e7919b2261fd7e96a94aa3db9cbfeb47d2225d
Elías Flores <153211931+eliflores12@users.noreply.github.com>
2025-06-24 14:07:56 -0600

    changes

--- resources/views/components/perfil.blade.php
+++ resources/views/components/perfil.blade.php
@@ -1,5 +1,34 @@
 <x-layout>
     <x-bar/>
 
+      <div class="flex justify-center items-center h-screen">
+            <div class="backdrop-blur-sm bg-white/30 rounded-md px-6 py-6">
+                <h1 class="text-6xl block text-center font-poppins">Perfil</h1>
+                <hr class="mt-3">
+
+                <form action="/login" method="POST" class="bg-gray shadow-md space-y-4 rounded px-8 pt-6 pb-8 mb-4">
+                    @csrf
+                    
+                        <x-input name="name" type="text">Nombre completo</x-input>
+                    </div>
+
+                    <div class="flex items-center">
+                    <span class="material-symbols-outlined">
+                        key
+                    </span>
+                    <x-input name="password" type="password">Contraseña</x-input>
+                    </div>
+
+                    <label class="flex items-center mt-2">
+                        <input type="checkbox" class="form-checkbox">
+                        <span class="ml-2 text-gray-600">Recuérdame</span>
+                    </label>
+                    <x-primary-button>Crear cuenta</x-primary-button>
+                </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
 
 </x-layout>
