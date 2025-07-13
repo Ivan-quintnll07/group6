c72c46c049c9ac01e35386f088148404c78f924b
Iván Quintanilla <153476509+Ivan-quintnll07@users.noreply.github.com>
2025-07-12 21:58:37 -0600

    w

--- resources/views/components/bar.blade.php
+++ resources/views/components/bar.blade.php
@@ -56,8 +56,7 @@
             <b><a href="calendar" style="display: inline-block; color: #f4f6fa; text-decoration: none; font-size: 18px; margin-bottom: 15px;">
                 <i class="fas fa-calendar-alt"></i> Calendario</a></b><br>
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
             <b><a href="addCard" style="display: inline-block; color: #f4f6fa; text-decoration: none; font-size: 18px; margin-bottom: 15px;">
                 <i class="fas fa-credit-card"></i> Agregar Tarjeta</a></b><br>
 
@@ -68,19 +67,17 @@
                 <i class="fa-solid fa-cart-shopping"></i> Favoritos</a></b><br>
 
             <b><a href="education" style="display: inline-block; color: #f4f6fa; text-decoration: none; font-size: 18px; margin-bottom: 15px;">
-=======
-=======
->>>>>>> Stashed changes
+
             <b><a href="education" style="display: inline-block; color: #60A5FA; text-decoration: none; font-size: 18px; margin-bottom: 15px;">
->>>>>>> Stashed changes
+
                 <i class="fas fa-graduation-cap"></i> Educación</a></b><br>
 
-<<<<<<< Updated upstream
-=======
+
+
                         <b><a href="favoritos" style="display: inline-block; color: #60A5FA; text-decoration: none; font-size: 18px; margin-bottom: 15px;">
                 <i class="fa-solid fa-cart-shopping"></i> favoritos</a></b><br>
 
->>>>>>> Stashed changes
+
 
 
             @endguest
@@ -104,24 +101,14 @@
             <b><a href="addCard" style="display: inline-block; color: #f4f6fa; text-decoration: none; font-size: 18px; margin-bottom: 10px;">
                 <i class="fas fa-credit-card"></i> Agregar Tarjeta</a></b><br>
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
              <b><a href="changeCard" style="display: inline-block; color: #f4f6fa; text-decoration: none; font-size: 18px; margin-bottom: 15px;">
                 <i class="fas fa-credit-card"></i> Cambiar Tarjeta</a></b><br>
 
-            <b><a href="favoritos" style="display: inline-block; color: #f4f6fa; text-decoration: none; font-size: 18px; margin-bottom: 15px;">
-                <i class="fa-solid fa-cart-shopping"></i> Favoritos</a></b><br>
 
-=======
-             <b><a href="changeCard" style="display: inline-block; color: #eeeff1; text-decoration: none; font-size: 18px; margin-bottom: 15px;">
-                <i class="fas fa-credit-card"></i> Cambiar Tarjeta</a></b><br>
 
->>>>>>> Stashed changes
-=======
-             <b><a href="changeCard" style="display: inline-block; color: #eeeff1; text-decoration: none; font-size: 18px; margin-bottom: 15px;">
-                <i class="fas fa-credit-card"></i> Cambiar Tarjeta</a></b><br>
 
->>>>>>> Stashed changes
+
 
             @endauth
         </nav>
